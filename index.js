35e98b8b7a02c1203a6820f8cf7fced2528b4700
savan <savannahwilliams243@gmail.com>
2023-11-18 16:00:55 -0800

    began questions in questions array

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a title!');
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
