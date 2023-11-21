02d27fdf89659175bed2196892c7accb66e9cf24
savan <savannahwilliams243@gmail.com>
2023-11-20 16:48:53 -0800

    added licenses details to separate js file

---
+++ utils/licensesDetails.js
@@ -0,0 +1,7 @@
+const licensesDetails = [
+    {
+      'name': 'Apache license 2.0',
+      'badge': `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+      'link': `https://opensource.org/licenses/Apache-2.0`
+    },
+]
