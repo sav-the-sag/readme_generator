630511e7eeb4d22b07e2ff21c88cfbfff0ae0761
savan <savannahwilliams243@gmail.com>
2023-11-20 17:07:49 -0800

    added more to licenses details

--- utils/licensesDetails.js
+++ utils/licensesDetails.js
@@ -1,7 +1,22 @@
 const licensesDetails = [
     {
-      'name': 'Apache license 2.0',
-      'badge': `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
-      'link': `https://opensource.org/licenses/Apache-2.0`
+        'name': 'Apache license 2.0',
+        'badge': `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+        'link': `https://opensource.org/licenses/Apache-2.0`
     },
+    {
+        'name': 'MIT',
+        'badge': `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+        'link': `https://opensource.org/licenses/MIT`
+    },
+    {
+        'name': 'Mozilla Public License 2.0',
+        'badge': `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
+        'link': `https://opensource.org/licenses/MPL-2.0`
+    },
+    {
+        'name': 'The Unlicense',
+        'badge': `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`,
+        'link': `http://unlicense.org/`
+    }
 ]
