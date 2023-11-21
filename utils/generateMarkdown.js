4041d8767f72bc8735122af72fccddd8bd0f7b56
savan <savannahwilliams243@gmail.com>
2023-11-20 16:53:20 -0800

    created license fucntion that returns the section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -21,7 +21,14 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == "None") {
+    return ``;
+  }
+  return `## License
+  This application is licensed under [${license}](${renderLicenseLink(license)}) license. Click the link for license rights and limitations.`;
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
